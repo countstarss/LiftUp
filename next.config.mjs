7c99984b17a9efefdb63c54d2685ff859599a80b
lukeking <countstarss@163.com>
2025-04-08 13:47:00 +0800

    fix: add missing supabase envs

--- next.config.mjs
+++ next.config.mjs
@@ -137,6 +137,10 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   images: {
     remotePatterns: [
       {
